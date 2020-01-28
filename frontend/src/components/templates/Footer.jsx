93fd95e80f05b152a7cb63cae250a5d9a8f7eca8
Gabriel Gomes <bibiel2605@gmail.com>
2020-01-28 18:51:09 -0300

    Add frontend

---
+++ frontend/src/components/templates/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            <i className="fa fa-empire text-danger"></i> Desenvolvido por
+            <strong> <a href="https://www.linkedin.com/in/gabriel-gomes-da-cunha-ab155b185/">Gabriel Gomes</a> </strong>
+        </span>
+    </footer>
---
+++ frontend/src/main/consts.js
@@ -0,0 +1,8 @@
+const API_URL = 'http://localhost:3001'
+
+const USER_INITIAL_STATE = {
+    user: { name: '', email: '' },
+    list: []
+}
+
+export { API_URL, USER_INITIAL_STATE }
